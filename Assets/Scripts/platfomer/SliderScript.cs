a09c2453e8ed215d4d7aa2e28f2196bc9b2e58c1
Sheesh <vladsolokov@gmail.com>
2023-12-27 20:49:26 +0300

    platformer 1

---
+++ Assets/Scripts/platfomer/SliderScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScript : MonoBehaviour
+{
+    private SliderJoint2D _slider;
+    private IEnumerator setSpeedMotor()
+    {
+        JointMotor2D jointMotor2D = new JointMotor2D();
+        jointMotor2D.motorSpeed = _slider.motor.motorSpeed == 5f ? -5f : 5f;
+        jointMotor2D.maxMotorTorque = 10000f;
+        _slider.motor = jointMotor2D;
+        yield return new WaitForSeconds(5f);
+
+        StartCoroutine(setSpeedMotor());
+    }
+
+    private void Start()
+    {
+        _slider = GetComponent<SliderJoint2D>();
+        StartCoroutine(setSpeedMotor());
+    }
+
+}
